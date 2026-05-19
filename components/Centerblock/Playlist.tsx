4a7260e7f65b68f19691a130bc542dbd4f86e5e9
Hygi1 <ppravu@mail.ru>
2026-05-19 13:51:47 +0700

    React Refs

--- components/Centerblock/Playlist.tsx
+++ components/Centerblock/Playlist.tsx
@@ -1,21 +1,12 @@
 "use client";
 
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/lib/store/store";
+import { setCurrentTrack, setPlaying } from "@/lib/store/playerSlice";
 import { data } from "@/lib/data";
 import Link from "next/link";
 import styles from "./Playlist.module.css";
-
-type Track = {
-  _id: number;
-  name: string;
-  author: string;
-  release_date: string;
-  genre: string[];
-  duration_in_seconds: number;
-  album: string;
-  logo: string | null;
-  track_file: string;
-  stared_user: string[];
-};
+import cn from "classnames";
 
 const formatTime = (seconds: number) => {
   const mins = Math.floor(seconds / 60);
@@ -24,44 +15,71 @@ const formatTime = (seconds: number) => {
 };
 
 export default function Playlist() {
+  const dispatch = useDispatch();
+  const { currentTrack, isPlaying } = useSelector(
+    (state: RootState) => state.player
+  );
+
+  const handleTrackClick = (track: (typeof data)[0]) => {
+    if (currentTrack?._id === track._id) {
+      dispatch(setPlaying(!isPlaying));
+    } else {
+      dispatch(setCurrentTrack(track));
+      dispatch(setPlaying(true));
+    }
+  };
+
   return (
     <div className={styles.playlist}>
-      {data.map((track: Track) => (
-        <div key={track._id} className={styles.playlist__item}>
-          <div className={styles.playlist__track}>
-            <div className={styles.track__title}>
-              <div className={styles.track__titleImage}>
-                <svg className={styles.track__titleSvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
-                </svg>
+      {data.map((track) => {
+        const isCurrent = currentTrack?._id === track._id;
+        return (
+          <div
+            key={track._id}
+            className={cn(styles.playlist__item, {
+              [styles.playing]: isCurrent && isPlaying,
+            })}
+            onClick={() => handleTrackClick(track)}
+          >
+            <div className={styles.playlist__track}>
+              <div className={styles.track__title}>
+                <div className={styles.track__titleImage}>
+                  {isCurrent && isPlaying ? (
+                    <div className={styles.pulsingDot}></div>
+                  ) : (
+                    <svg className={styles.track__titleSvg}>
+                      <use xlinkHref="/img/icon/sprite.svg#icon-note" />
+                    </svg>
+                  )}
+                </div>
+                <div className={styles.track__titleText}>
+                  <Link href="#" className={styles.track__titleLink}>
+                    {track.name}
+                  </Link>
+                </div>
               </div>
-              <div className={styles.track__titleText}>
-                <Link href="#" className={styles.track__titleLink}>
-                  {track.name}
+              <div className={styles.track__author}>
+                <Link href="#" className={styles.track__authorLink}>
+                  {track.author}
                 </Link>
               </div>
-            </div>
-            <div className={styles.track__author}>
-              <Link href="#" className={styles.track__authorLink}>
-                {track.author}
-              </Link>
-            </div>
-            <div className={styles.track__album}>
-              <Link href="#" className={styles.track__albumLink}>
-                {track.album}
-              </Link>
-            </div>
-            <div className={styles.track__time}>
-              <svg className={styles.track__timeSvg}>
-                <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
-              </svg>
-              <span className={styles.track__timeText}>
-                {formatTime(track.duration_in_seconds)}
-              </span>
+              <div className={styles.track__album}>
+                <Link href="#" className={styles.track__albumLink}>
+                  {track.album}
+                </Link>
+              </div>
+              <div className={styles.track__time}>
+                <svg className={styles.track__timeSvg}>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-like" />
+                </svg>
+                <span className={styles.track__timeText}>
+                  {formatTime(track.duration_in_seconds)}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
--- components/Player/Player.tsx
+++ components/Player/Player.tsx
@@ -1,69 +1,184 @@
 "use client";
 
-import Link from "next/link";
+import { useRef, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/lib/store/store";
+import {
+  setCurrentTime,
+  setDuration,
+  setPlaying,
+  setVolume,
+  togglePlay,
+} from "@/lib/store/playerSlice";
 import styles from "./Player.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Player() {
+  const dispatch = useDispatch();
+  const { currentTrack, isPlaying, currentTime, duration, volume } =
+    useSelector((state: RootState) => state.player);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume;
+    }
+  }, [volume]);
+
+  useEffect(() => {
+    if (audioRef.current && currentTrack) {
+      if (isPlaying) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [isPlaying, currentTrack]);
+
+  const handleTimeUpdate = () => {
+    if (audioRef.current) {
+      dispatch(setCurrentTime(audioRef.current.currentTime));
+    }
+  };
+
+  const handleLoadedMetadata = () => {
+    if (audioRef.current) {
+      dispatch(setDuration(audioRef.current.duration));
+    }
+  };
+
+  const handleSeek = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const time = parseFloat(e.target.value);
+    if (audioRef.current) {
+      audioRef.current.currentTime = time;
+      dispatch(setCurrentTime(time));
+    }
+  };
+
+  const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const vol = parseFloat(e.target.value);
+    dispatch(setVolume(vol));
+    if (audioRef.current) {
+      audioRef.current.volume = vol;
+    }
+  };
+
+  const formatTime = (seconds: number) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
+  };
+
+  if (!currentTrack) {
+    return (
+      <div className={styles.bar}>
+        <div className={styles.barContent}>
+          <div className={styles.barPlayerProgress}></div>
+          <div className={styles.barPlayerBlock}>
+            <div className={styles.barPlayer}></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.bar}>
+      <audio
+        ref={audioRef}
+        src={currentTrack.track_file}
+        onTimeUpdate={handleTimeUpdate}
+        onLoadedMetadata={handleLoadedMetadata}
+        onEnded={() => dispatch(setPlaying(false))}
+      />
       <div className={styles.barContent}>
-        <div className={styles.barPlayerProgress}></div>
+        <div className={styles.barPlayerProgress}>
+          <input
+            type="range"
+            min="0"
+            max={duration}
+            value={currentTime}
+            onChange={handleSeek}
+            className={styles.progressLine}
+            aria-label="Прогресс трека"
+          />
+        </div>
         <div className={styles.barPlayerBlock}>
           <div className={styles.barPlayer}>
             <div className={styles.playerControls}>
               <div className={styles.playerBtnPrev}>
                 <svg className={styles.playerBtnPrevSvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-prev"></use>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-prev" />
                 </svg>
               </div>
-              <div className={styles.playerBtnPlay}>
-                <svg className={styles.playerBtnPlaySvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
-                </svg>
+              <div
+                className={styles.playerBtnPlay}
+                onClick={() => dispatch(togglePlay())}
+              >
+                {isPlaying ? (
+                  <svg className={styles.playerBtnPlaySvg}>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-pause" />
+                  </svg>
+                ) : (
+                  <svg className={styles.playerBtnPlaySvg}>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-play" />
+                  </svg>
+                )}
               </div>
               <div className={styles.playerBtnNext}>
                 <svg className={styles.playerBtnNextSvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-next"></use>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-next" />
                 </svg>
               </div>
               <div className={styles.playerBtnRepeat}>
                 <svg className={styles.playerBtnRepeatSvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-repeat"></use>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-repeat" />
                 </svg>
               </div>
               <div className={styles.playerBtnShuffle}>
                 <svg className={styles.playerBtnShuffleSvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-shuffle"></use>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-shuffle" />
                 </svg>
               </div>
             </div>
             <div className={styles.playerTrackPlay}>
               <div className={styles.trackPlayContain}>
                 <div className={styles.trackPlayImage}>
-                  <svg className={styles.trackPlaySvg}>
-                    <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
-                  </svg>
+                  {currentTrack.logo ? (
+                    <Image
+                      src={currentTrack.logo}
+                      alt="track"
+                      width={51}
+                      height={51}
+                    />
+                  ) : (
+                    <svg className={styles.trackPlaySvg}>
+                      <use xlinkHref="/img/icon/sprite.svg#icon-note" />
+                    </svg>
+                  )}
                 </div>
                 <div className={styles.trackPlayAuthor}>
                   <Link href="#" className={styles.trackPlayAuthorLink}>
-                    Ты та...
+                    {currentTrack.name}
                   </Link>
                 </div>
                 <div className={styles.trackPlayAlbum}>
                   <Link href="#" className={styles.trackPlayAlbumLink}>
-                    Баста
+                    {currentTrack.author}
                   </Link>
                 </div>
               </div>
               <div className={styles.trackPlayLikeDis}>
                 <div className={styles.trackPlayLike}>
                   <svg className={styles.trackPlayLikeSvg}>
-                    <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-like" />
                   </svg>
                 </div>
                 <div className={styles.trackPlayDislike}>
                   <svg className={styles.trackPlayDislikeSvg}>
-                    <use xlinkHref="/img/icon/sprite.svg#icon-dislike"></use>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-dislike" />
                   </svg>
                 </div>
               </div>
@@ -73,14 +188,18 @@ export default function Player() {
             <div className={styles.volumeContent}>
               <div className={styles.volumeImage}>
                 <svg className={styles.volumeSvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-volume"></use>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-volume" />
                 </svg>
               </div>
               <div className={styles.volumeProgress}>
                 <input
-                  className={styles.volumeProgressLine}
                   type="range"
-                  name="range"
+                  min="0"
+                  max="1"
+                  step="0.01"
+                  value={volume}
+                  onChange={handleVolumeChange}
+                  className={styles.volumeProgressLine}
                   aria-label="Громкость"
                 />
               </div>
